33a7139d851965eab745c87064998e1afe094b65
Santiago Rincon <makekill0107@gmail.com>
2019-12-03 14:07:50 -0500

    cambio

--- index.php
+++ index.php
@@ -50,11 +50,9 @@ if(isset($accessToken)){
         'oauth_uid'     => $fbUserProfile['id'],
         'first_name'    => $fbUserProfile['first_name'],
         'last_name'     => $fbUserProfile['last_name'],
-        'email'         => $fbUserProfile['email'],
-        'gender'        => $fbUserProfile['gender'],
-        'locale'        => $fbUserProfile['locale'],
+        
         'picture'       => $fbUserProfile['picture']['url'],
-        'link'          => $fbUserProfile['link']
+        
     );
     $userData = $user->checkUser($fbUserData);
     
